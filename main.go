dd16364e441df1313da86e85afffce69dfc01a99
Marcel Pohland <m.pohland@mwaysolutions.com>
2019-08-23 13:24:54 +0200

    reduce main

--- main.go
+++ main.go
@@ -112,28 +112,6 @@ func main() {
 
 	var echoTimes int
 
-	var cmdPrint = &cobra.Command{
-		Use:   "print [string to print]",
-		Short: "Print anything to the screen",
-		Long: `print is for printing anything back to the screen.
-For many years people have printed back to the screen.`,
-		Args: cobra.MinimumNArgs(1),
-		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println("Print: " + strings.Join(args, " "))
-		},
-	}
-
-	var cmdEcho = &cobra.Command{
-		Use:   "echo [string to echo]",
-		Short: "Echo anything to the screen",
-		Long: `echo is for echoing anything back.
-Echo works a lot like print, except it has a child command.`,
-		Args: cobra.MinimumNArgs(1),
-		Run: func(cmd *cobra.Command, args []string) {
-			fmt.Println("Print: " + strings.Join(args, " "))
-		},
-	}
-
 	var cmdTimes = &cobra.Command{
 		Use:   "times [string to echo]",
 		Short: "Echo anything to the screen more times",
@@ -151,12 +129,12 @@ a count and a string.`,
 
 	var rootCmd = &cobra.Command{Use: "go-gaiad-updater"}
 	rootCmd.AddCommand(
-		cmdEcho,
-		cmdPrint,
+		cmd.CmdEcho,
+		cmd.CmdPrint,
 		cmd.VersionCmd,
 		cmd.ConfigCmd,
 		cmd.CreateCmd,
 		cmd.ServeCmd)
-	cmdEcho.AddCommand(cmdTimes)
+	cmd.CmdEcho.AddCommand(cmdTimes)
 	rootCmd.Execute()
 }
