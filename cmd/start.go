ece8ff224b43ba8872795ab58a07e1c12c5309ff
Marcel Pohland <m.pohland@mwaysolutions.com>
2019-08-23 16:24:16 +0200

    update test successful

--- cmd/start.go
+++ cmd/start.go
@@ -22,11 +22,13 @@ THE SOFTWARE.
 package cmd
 
 import (
+	"bytes"
 	"fmt"
 	"github.com/mitchellh/go-homedir"
-	"os"
-
 	"github.com/spf13/cobra"
+	"log"
+	"os"
+	"os/exec"
 )
 
 // startCmd represents the start command
@@ -40,17 +42,26 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("start called")
 		home, err := homedir.Dir()
 		if err != nil {
 			fmt.Println(err)
 			os.Exit(1)
 		}
-		fmt.Println("YOUR HOME address: " + home);
+		home = home + "/go/src/github.com/cosmos/gaia/"
+		fmt.Println("YOUR HOME address: " + home)
+		GitFetchCommand(home)
+		GitCheckoutCommand(home)
+		GitCheckoutVersionCommand(home)
+		GitCheckoutCleanFDCommand(home)
+		GitCheckoutCleanFXCommand(home)
+		GoVersionCheck(home)
+		MakeGoModCache(home)
+		MakeInstall(home)
 	},
 }
 
 func init() {
+
 	//rootCmd.AddCommand(startCmd)
 
 	// Here you will define your flags and configuration settings.
@@ -62,4 +73,99 @@ func init() {
 	// Cobra supports local flags which will only run when this command
 	// is called directly, e.g.:
 	// startCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+func GitFetchCommand(home string) {
+	cmd := exec.Command("git", "fetch", "--all")
+	cmd.Dir = home
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	err := cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("in all caps: %q\n", out.String())
+}
+func GitCheckoutCommand(home string) {
+	cmd := exec.Command("git", "checkout", ".")
+	cmd.Dir = home
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	err := cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("in all caps: %q\n", out.String())
+}
+
+func GitCheckoutVersionCommand(home string) {
+	cmd := exec.Command("git", "checkout", "v2.0.0")
+	cmd.Dir = home
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	err := cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("in all caps: %q\n", out.String())
+}
+
+func GitCheckoutCleanFDCommand(home string) {
+	cmd := exec.Command("git", "clean", "-fd")
+	cmd.Dir = home
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	err := cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("in all caps: %q\n", out.String())
+}
+
+func GitCheckoutCleanFXCommand(home string) {
+	cmd := exec.Command("git", "clean", "-fx")
+	cmd.Dir = home
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	err := cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("in all caps: %q\n", out.String())
+}
+
+func GoVersionCheck(home string) {
+	cmd := exec.Command("go", "version")
+	cmd.Dir = home
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	err := cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("GOVERSION: %q\n", out.String())
+}
+
+func MakeGoModCache(home string) {
+	cmd := exec.Command("make", "go-mod-cache")
+	cmd.Dir = home
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	err := cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Log: %q\n", out.String())
+}
+
+func MakeInstall(home string) {
+	cmd := exec.Command("make", "install")
+	cmd.Dir = home
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	err := cmd.Run()
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Log: %q\n", out.String())
 }
