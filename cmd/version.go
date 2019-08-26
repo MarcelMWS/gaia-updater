beebbcd08e03a1e265419c97489a1d4db5013010
Marcel Pohland <m.pohland@mwaysolutions.com>
2019-08-26 11:36:20 +0200

    finish gaiaBuildPath param

--- cmd/version.go
+++ cmd/version.go
@@ -30,15 +30,10 @@ import (
 // versionCmd represents the version command
 var VersionCmd = &cobra.Command{
 	Use:   "version",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "print this version",
+	Long: `print this version`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("version called")
+		fmt.Println("v0.0.1")
 	},
 }
 
