5369e7ad3bf86a43b6b18108eeac4443a1ddc08e
Marcel Pohland <m.pohland@mwaysolutions.com>
2019-08-26 15:41:24 +0200

    typos

--- cmd/version.go
+++ cmd/version.go
@@ -27,13 +27,12 @@ import (
 	"github.com/spf13/cobra"
 )
 
-// versionCmd represents the version command
 var VersionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "print version",
 	Long: `print this version`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("v0.0.1-rc2")
+		fmt.Println("v0.1.0-rc1")
 	},
 }
 
