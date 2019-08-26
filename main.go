d977c0dfce84fbbbe999c7db37a1b5f92449a288
Marcel Pohland <m.pohland@mwaysolutions.com>
2019-08-26 10:39:47 +0200

    write help pages

--- main.go
+++ main.go
@@ -38,13 +38,10 @@ var userLicense string
 // rootCmd represents the root command
 var rootCmd = &cobra.Command{
 	Use:   "go-gaiad-updater",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+	Short: "update cosmos-sdk-gaia repository",
+	Long: `Update and compile cosmos-sdk-gaia repository:
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+You have to specify the git version/tag to checkout and compile the right version`,
 }
 
 func Execute() {
