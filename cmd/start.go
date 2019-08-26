99041dd280b80c9037c4d004cd0284ca32ee849d
Marcel Pohland <m.pohland@mwaysolutions.com>
2019-08-26 10:39:47 +0200

    write help pages

--- cmd/start.go
+++ cmd/start.go
@@ -34,13 +34,8 @@ import (
 // startCmd represents the start command
 var StartCmd = &cobra.Command{
 	Use:   "start",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "start update",
+	Long: `start update and specify version`,
 	Run: func(cmd *cobra.Command, args []string) {
 		home, err := homedir.Dir()
 		if err != nil {
